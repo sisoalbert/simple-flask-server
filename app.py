8eef97f38dedc0ee864fd0d270cb503bde03dd9d
Siso <sisoalbert@yahoo.com>
2024-07-17 22:08:13 +0200

    added API KEY SECURITY

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 import json
+from dotenv import load_dotenv
+import os
+load_dotenv() 
 
 app = Flask(__name__)
 CORS(app)
@@ -9,16 +12,27 @@ CORS(app)
 with open('assets/labs.json') as f:
     labs = json.load(f)
 
+# Define a list of valid API keys
+VALID_API_KEYS = {os.getenv("VALID_API_KEY")}
+
+def check_api_key():
+    api_key = request.headers.get('x-api-key')
+    if api_key not in VALID_API_KEYS:
+         abort(403, description="Invalid API key")
+    
+
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({"message": "Hello, World!"})
 
 @app.route('/labs', methods=['GET'])
 def get_labs():
+    check_api_key()
     return jsonify(labs)
 
 @app.route('/labs/<string:lab_id>', methods=['GET'])
 def get_lab_by_id(lab_id):
+    check_api_key()
     lab = next((lab for lab in labs if lab["id"] == lab_id), None)
     if lab:
         return jsonify(lab)
